1a1ae0e40f57a9fb1f929100df2394b68242035c
Fulvio Stefanine <62194735+fustsiju@users.noreply.github.com>
2022-07-12 13:01:12 -0300

    Add files via upload

---
+++ promiseFetch.js
@@ -0,0 +1,6 @@
+fetch('https://api.github.com/users/fustsiju')
+.then((response) => console.log(response))
+// fetch("https://api.github.com/users/fustsiju").then((response) =>
+//  console.log(response)
+//  );
+  
